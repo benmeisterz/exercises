027069476e1a43e6365fb33606f0f0725b16b88c
benmeisterz <ben.naldoza@gmail.com>
2023-06-27 12:14:03 +0800

    solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
 
 import './App.css'
 
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA"
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D"
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF"
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33"
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB"
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00"
+  }
+];
+
 function App() {
-
-
   return (
     <div className="card">
       <Avatar />
@@ -30,16 +61,18 @@ function Intro() {
 
 function SkillList() {
   return (
-    <div className='skill-list'>
-      <Skill skill="js" emoji="💪" color="blue" />
+    <ul className='skill-list'>
+      {skills.map(skill => (
+        <li style={{ background: skill.color }}> {skill.skill} {skill.level} { } </li>))}
+      {/* <Skill skill="js" emoji="💪" color="blue" />
       <Skill skill="react" emoji="👶" color="green" />
-      <Skill skill="css" emoji="😅" color="yellow" />
-    </div>
+      <Skill skill="css" emoji="😅" color="yellow" /> */}
+    </ul>
   )
 }
 
-function Skill({ skill, emoji, color }) {
-  return <div className='skill' style={{ background: color }}>{skill} {emoji}</div>
-}
+// function Skill({ skill, emoji, color }) {
+//   return <div className='skill' style={{ background: color }}>{skill} {emoji}</div>
+// }
 
 export default App
